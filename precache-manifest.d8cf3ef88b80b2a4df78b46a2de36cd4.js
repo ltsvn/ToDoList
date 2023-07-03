094874273829ce94efb31922b6ef1560e13a16f7
ltsvn <92997990+ltsvn@users.noreply.github.com>
2023-07-03 10:47:03 +0200

    Updates

---
+++ precache-manifest.d8cf3ef88b80b2a4df78b46a2de36cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7dfd22f950cf9d7453e9010bb864b96",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "2a037ba69f46188c1b6c",
+    "url": "/ToDoList/static/css/main.3cdc7b26.chunk.css"
+  },
+  {
+    "revision": "4cc34447e90addbf5bfa",
+    "url": "/ToDoList/static/js/2.30076789.chunk.js"
+  },
+  {
+    "revision": "c18546fe0cf2182f83e5d04d0130ce43",
+    "url": "/ToDoList/static/js/2.30076789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a037ba69f46188c1b6c",
+    "url": "/ToDoList/static/js/main.9a9b029d.chunk.js"
+  },
+  {
+    "revision": "86c4286c12cc5c044c93",
+    "url": "/ToDoList/static/js/runtime-main.1aff77a7.js"
+  }
+]);
